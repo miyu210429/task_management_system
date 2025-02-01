4cfcf2426cb373bc3a79981ee4f967826d1d3ef9
akinori <a.kanda0310@gmail.com>
2025-02-01 18:04:02 +0900

    https://github.com/miyu210429/task_management_system/issues/2 これの実装

---
+++ accounts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>アカウント一覧</title>
+  <link rel="stylesheet" href="/css/common.css">
+</head>
+<body>
+  <div class="container">
+    <?php include './template/default.php'; ?>
+    <main class="main-content">
+    </main>
+
+
+  </div>
+</body>
+</html>
