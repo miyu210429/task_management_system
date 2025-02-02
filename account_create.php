d8704791fc85287166d2ae9c7e1b659410425c63
akinori <a.kanda0310@gmail.com>
2025-02-02 13:50:06 +0900

    https://github.com/miyu210429/task_management_system/issues/15 これの対応およびリンク修正

--- account_create.php
+++ account_create.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>アカウント作成</title>
+  <title>アカウント登録</title>
   <link rel="stylesheet" href="/css/common.css">
   <link rel="stylesheet" href="/css/account.css">
 </head>
--- template/default.php
+++ template/default.php
@@ -6,7 +6,7 @@
   <nav class="menu">
     <ul>
       <li><a href="/task_list.php">タスク一覧</a></li>
-      <li><a href="/">新規タスク作成</a></li>
+      <li><a href="/task_create.php">新規タスク作成</a></li>
       <li><a href="/account_list.php">アカウント一覧</a></li>
       <li><a href="/logout.php">ログアウト</a></li>
     </ul>
