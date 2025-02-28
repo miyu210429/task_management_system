fc4e50818d0fafd7880cd3541b393827705d412b
神田　美優 <k.miyu210429@gmail.com>
2025-02-28 21:49:10 +0900

    最後の修正

--- app/auth.php
+++ app/auth.php
@@ -5,14 +5,13 @@ require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/classes/User.php';
 
 //ログインしているかチェック
-//リクアイアすると、$_SESSIONは常に存在していないから、login.phpにずっと飛ばされる
 //login.phpのときは$_SESSION['User']['id']が存在していないかチェックしない
 if($_SERVER['REQUEST_URI'] != '/login.php' && !isset($_SESSION['User']['id'])) {
     header("Location: /login.php");
     exit();
 }
 
-//ログアウトしていないユーザーが来た場合はタスク一覧ページにとばす
+//ログアウトしていないユーザーがログインページに来た場合はタスク一覧ページにとばす
 if($_SERVER['REQUEST_URI'] == '/login.php' && isset($_SESSION['User']['id'])){
     header('Location: task_list.php'); exit();
 }
@@ -25,7 +24,7 @@ if(isset($_SESSION['User']['id'])){
     $login_user = $user->getById($_SESSION['User']['id']);
     $user = null; //本来はこういう書き方しないけど後々$userでインスタンス作ってるのでこのインスタンスはここで破棄
 
-    //ユーザーが見つからない場合はログアウト処理
+    //ユーザーが見つからない場合はログインページへ
     if (!$login_user) {
         session_destroy();
         header("Location: /login.php");
